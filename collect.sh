05e122d3c8916916594fe9a2183cecad6ecdafce
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2021-12-24 19:49:04 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -3,7 +3,7 @@ cd /tmp/rom
 
 . build/envsetup.sh
 lunch superior_juice-userdebug
-export CCACHE_DIR=/tmp/superior/ccache
+export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 20G
