20ab790eef0e98dd93ca4b65e760636df27f875e
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2021-12-26 02:30:21 +0700

    Update final.sh

--- final.sh
+++ final.sh
@@ -7,10 +7,6 @@ export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-export BUILD_USER=lynx
-export BUILD_HOST=Serber-UWU
-export BUILD_USERNAME=LynZx
-export BUILD_HOSTNAME=Serber-UWU
 
 . build/envsetup.sh
 lunch nad_juice-userdebug
