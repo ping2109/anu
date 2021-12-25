504522dbcff3772b4a37da07ea61c0204cadafc7
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2021-12-26 02:29:18 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -3,13 +3,13 @@ cd /tmp/rom
 
 . build/envsetup.sh
 lunch nad_juice-userdebug
-export CCACHE_DIR=/tmp/superior/ccache
+export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
 make nad -j8 &
-sleep 30m
+sleep 90m
 kill %1
 ccache -s
