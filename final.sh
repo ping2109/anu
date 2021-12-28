570f4a991664b843907e290584519acddf6263f0
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2021-12-29 04:50:55 +0700

    Update final.sh

--- final.sh
+++ final.sh
@@ -9,7 +9,7 @@ ccache -o compression=true
 ccache -z
 
 . build/envsetup.sh
-lunch nad_ysl-userdebug
+lunch nad_juice-userdebug
 export BUILD_USERNAME=lynx
 export BUILD_HOSTNAME=serber-uwu
 export TZ=Asia/Jakarta
