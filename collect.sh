3ea085466244f8beb081b5e1b0019faf456833a8
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2021-12-31 02:21:45 +0700

    Collect CCACHE EvoX Juice

--- collect.sh
+++ collect.sh
@@ -2,14 +2,14 @@
 cd /tmp/rom
 
 . build/envsetup.sh
-lunch nad_juice-userdebug
+lunch evolution_juice-user
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-make nad -j8 &
+mka evolution -j8 &
 sleep 90m
 kill %1
 ccache -s
