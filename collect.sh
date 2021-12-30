39286640191e55d5ffff72b7532749271427b26d
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2021-12-31 03:21:53 +0700

    Collect CCACHE Nad Juice

--- collect.sh
+++ collect.sh
@@ -3,6 +3,10 @@ cd /tmp/rom
 
 . build/envsetup.sh
 lunch nad_juice-userdebug
+export BUILD_USERNAME=lynx
+export BUILD_HOSTNAME=serber-uwu
+export KBUILD_BUILD_USER=LynZx
+export KBUILD_BUILD_HOST=Serber-UWU
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
