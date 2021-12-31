52bfc44d785baac438a6cd74d0b61c6cf85efec4
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-01-01 05:48:50 +0700

    Collect CCACHE PalladiumOS Twelve

--- collect.sh
+++ collect.sh
@@ -2,19 +2,17 @@
 cd /tmp/rom
 
 . build/envsetup.sh
-lunch evolution_juice-user
-export BUILD_USERNAME=lynx
-export BUILD_HOSTNAME=serber-uwu
-export KBUILD_BUILD_USER=LynZx
-export KBUILD_BUILD_HOST=Serber-UWU
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch palladium_juice-userdebug
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-mka evolution -j8 &
+export KBUILD_BUILD_USER=LynZx
+export KBUILD_BUILD_HOST=Serber-UWU
+export SELINUX_IGNORE_NEVERALLOWS=true
+mka palladium -j8 &
 sleep 90m
 kill %1
 ccache -s
