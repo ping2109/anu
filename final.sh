6edc6c1b9afd730fe5f541e4025e7c933394df21
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-01-01 05:46:43 +0700

    Update final.sh

--- final.sh
+++ final.sh
@@ -9,11 +9,11 @@ ccache -o compression=true
 ccache -z
 
 . build/envsetup.sh
-lunch evolution_juice-user
+lunch palladium_juice-userdebug
 export BUILD_USERNAME=lynx
 export BUILD_HOSTNAME=serber-uwu
 export KBUILD_BUILD_USER=LynZx
 export KBUILD_BUILD_HOST=Serber-UWU
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta
-mka evolution -j8
+mka palladium -j8
