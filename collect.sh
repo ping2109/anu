67dfc42ceb1316ad00c7da85e4e6de9541f0a18a
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-01-02 04:44:24 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -9,8 +9,13 @@ export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
+export BUILD_USERNAME=lynx
+export BUILD_HOSTNAME=serber-uwu
+export KBUILD_BUILD_USER=LynZx
+export KBUILD_BUILD_HOST=Serber-UWU
+export TZ=Asia/Jakarta
 mka evolution -j8 &
-sleep 30m
+sleep 90m
 kill %1
 ccache -s
 
