fdaf6c200b745809b6dd0c7cddd6072dcdb849ca
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-01-03 04:54:34 +0700

    Update final.sh

--- final.sh
+++ final.sh
@@ -9,7 +9,7 @@ ccache -o compression=true
 ccache -z
 
 . build/envsetup.sh
-lunch evolution_juice-user
+lunch evolution_juice-userdebug
 export BUILD_USERNAME=lynx
 export BUILD_HOSTNAME=serber-uwu
 export KBUILD_BUILD_USER=LynZx
