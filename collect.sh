765519cc6e894914ce56f7b5f9f0a6ccca7e9b30
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-01-03 13:50:21 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -2,7 +2,7 @@
 cd /tmp/rom
 
 . build/envsetup.sh
-lunch evolution_juice-userdebug
+lunch conquer_juice-userdebug
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
@@ -14,7 +14,7 @@ export BUILD_HOSTNAME=serber-uwu
 export KBUILD_BUILD_USER=LynZx
 export KBUILD_BUILD_HOST=Serber-UWU
 export TZ=Asia/Jakarta
-mka evolution -j8 &
+mka carthage -j8 &
 sleep 90m
 kill %1
 ccache -s
