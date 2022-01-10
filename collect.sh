1c27b7bf96e40d16efbab1773542c73bb8ddac8e
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-01-10 19:18:50 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 cd /tmp/rom
 
-. build/envsetup.sh
-lunch nad_juice-userdebug
+source build/envsetup.sh
+lunch stormbreaker_juice-userdebug
 export BUILD_USERNAME=lynx
 export BUILD_HOSTNAME=serber-uwu
 export KBUILD_BUILD_USER=LynZx
 export KBUILD_BUILD_HOST=Serber-UWU
+export TZ=Asia/Jakarta
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-make nad -j8 &
-sleep 90m
+make storm-prod -j8 &
+sleep 30m
 kill %1
 ccache -s
