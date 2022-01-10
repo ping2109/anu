c186f6a1659881498456831355849640e47d929d
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-01-11 03:40:03 +0700

    Test LegionOS 12

--- collect.sh
+++ collect.sh
@@ -2,7 +2,7 @@
 cd /tmp/rom
  
 . build/envsetup.sh
-lunch conquer_juice-userdebug
+lunch legion_juice-userdebug
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
@@ -14,7 +14,7 @@ export BUILD_HOSTNAME=serber-uwu
 export KBUILD_BUILD_USER=LynZx
 export KBUILD_BUILD_HOST=Serber-UWU
 export TZ=Asia/Jakarta
-make carthage -j8 &
+make legion -j8 &
 sleep 30m
 kill %1
 ccache -s
