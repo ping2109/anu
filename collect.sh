db7edc8adac40191813905494c4375806ee2f00d
lynnnnzx <rifqikanekii140202@gmail.com>
2022-01-28 16:08:48 +0700

    CCACHE Kasumi v1
    Signed-off-by: lynnnnzx <rifqikanekii140202@gmail.com>

--- collect.sh
+++ collect.sh
@@ -2,18 +2,14 @@
 cd /tmp/rom
 
 . build/envsetup.sh
-lunch nad_juice-user
-export BUILD_USERNAME=lynx
-export BUILD_HOSTNAME=cirrus-ci
-export KBUILD_BUILD_USER=LynZx
-export KBUILD_BUILD_HOST=Cirrus-CI
+lunch kasumi_juice-userdebug
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-make nad -j8 &
+mka bandori -j8 &
 sleep 90m
 kill %1
 ccache -s
--- final.sh
+++ final.sh
@@ -9,10 +9,8 @@ ccache -o compression=true
 ccache -z
 
 . build/envsetup.sh
-lunch nad_juice-user
+lunch kasumi_juice-userdebug
 export BUILD_USERNAME=lynx
-export BUILD_HOSTNAME=cirrus-ci
-export KBUILD_BUILD_USER=LynZx
-export KBUILD_BUILD_HOST=Cirrus-CI
+export BUILD_HOSTNAME=serbersangar
 export TZ=Asia/Jakarta
-mka nad -j8
+mka bandori -j8
