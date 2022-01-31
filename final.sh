2bfb737432e02b9af1b42ffa27b21260512c2cc4
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-01-31 22:24:53 +0700

    Final Tes

--- final.sh
+++ final.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 cd /tmp/rom
 
+. build/envsetup.sh
+lunch lineage_juice-userdebug
+export BUILD_USERNAME=lynx
+export BUILD_HOSTNAME=serber-memek
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-
-. build/envsetup.sh
-lunch lineage_juice-userdebug
-export BUILD_USERNAME=lynx
-export BUILD_HOSTNAME=serber-uwu
 export TZ=Asia/Jakarta
-mka bacon -j8
+mka bacon -j$(nproc all)
