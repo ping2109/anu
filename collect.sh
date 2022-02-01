f6a5a8c1dc3f91ad2b0bd7d71f285e1f5fd3499d
lynnnnzx <rifqikanekii140202@gmail.com>
2022-02-01 15:43:13 +0700

    Test Los-FE
    Signed-off-by: lynnnnzx <rifqikanekii140202@gmail.com>

--- collect.sh
+++ collect.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 cd /tmp/rom
 
-. build/envsetup.sh
-lunch nad_juice-user
+source build/envsetup.sh
+lunch lineage_juice-userdebug
 export BUILD_USERNAME=lynx
-export BUILD_HOSTNAME=cirrus-ci
-export KBUILD_BUILD_USER=LynZx
-export KBUILD_BUILD_HOST=Cirrus-CI
+export BUILD_HOSTNAME=serber-memek
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-make nad -j8 &
+make bacon -j8 &
 sleep 90m
 kill %1
 ccache -s
--- final.sh
+++ final.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 cd /tmp/rom
 
+source build/envsetup.sh
+lunch lineage_juice-userdebug
+export BUILD_USERNAME=lynx
+export BUILD_HOSTNAME=serber-memek
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-
-. build/envsetup.sh
-lunch nad_juice-user
-export BUILD_USERNAME=lynx
-export BUILD_HOSTNAME=cirrus-ci
-export KBUILD_BUILD_USER=LynZx
-export KBUILD_BUILD_HOST=Cirrus-CI
-export TZ=Asia/Jakarta
-mka nad -j8
+make bacon -j8 &
