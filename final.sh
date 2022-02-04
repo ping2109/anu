e1b644f794f5d4059b23a401ae1e1d7a2a361ece
BagasSep21 <91189529+BagasSep21@users.noreply.github.com>
2022-02-04 13:40:31 +0700

    Update final.sh

--- final.sh
+++ final.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /tmp/rom
 
-source build/envsetup.sh
-lunch lineage_juice-userdebug
+. build/envsetup.sh
+lunch nad_juice-userdebug
 export BUILD_USERNAME=lynx
 export BUILD_HOSTNAME=serber-memek
 export CCACHE_DIR=/tmp/ccache
@@ -11,4 +11,4 @@ export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-make bacon -j8
+mka nad -j8
