62bffc21d99ab534100e24c3a005914dbdfeb762
imannig <akugedekntl@gmail.com>
2022-02-10 16:03:51 +0700

    gapeduli commit messeage

--- final.sh
+++ final.sh
@@ -2,7 +2,7 @@
 cd /tmp/rom
 
 . build/envsetup.sh
-lunch rr_juice-userdebug
+$lunch
 export BUILD_USERNAME=imannig
 export BUILD_HOSTNAME=cirrus-kontol
 export CCACHE_DIR=/tmp/ccache
@@ -11,4 +11,4 @@ export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-mka bacon -j8
+$make -j8
