c6c1c629d90f08f95912cfdb3da163e586cf7a2c
imannig <akugedekntl@gmail.com>
2022-02-20 15:06:22 +0700

    y

--- collect.sh
+++ collect.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-cd /tmp/rom
+cd ~/rom
 
 . build/envsetup.sh
-$lunch
 export BUILD_USERNAME=imannig
 export BUILD_HOSTNAME=cirrus-kontol
-export CCACHE_DIR=/tmp/ccache
+export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-ccache -M 100G -F 0
-ccache -o compression=true
-ccache -z
-$make  -j8 &
-sleep 90m
+ccache -M 8G -F 0
+$lunch
+$make -j8 &
+sleep 100m
 kill %1
 ccache -s
--- final.sh
+++ final.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-cd /tmp/rom
+cd ~/rom
 
 . build/envsetup.sh
-$lunch
 export BUILD_USERNAME=imannig
 export BUILD_HOSTNAME=cirrus-kontol
-export CCACHE_DIR=/tmp/ccache
+export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-ccache -M 20G
+ccache -M 20G -F 0
 ccache -o compression=true
 ccache -z
+
+$lunch
 $make -j8
