cdef9d7541b76640b02dac1818bf7dcf9c486b4f
pingmado <75196272+ping2109@users.noreply.github.com>
2022-03-17 13:13:34 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -2,6 +2,7 @@
 cd ~/rom
 
 . build/envsetup.sh
+
 export BUILD_USERNAME=ping2109
 export BUILD_HOSTNAME=freefire-server
 export CCACHE_DIR=~/ccache
@@ -9,8 +10,9 @@ export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=false
+mka clean && mka api-stubs-docs && mka hiddenapi-lists-docs && mka system-api-stubs-docs && mka test-api-stubs-docs
 ccache -M 8G -F 0
 $lunch
-$make -j16 &
+$mka bacon -j16 &
 kill %1
 ccache -s
