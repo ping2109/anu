f6e5ba20b4a33f4b3d3180551ee46eb6f5f0dc07
pingmado <75196272+ping2109@users.noreply.github.com>
2022-04-16 05:42:32 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -8,10 +8,14 @@ export BUILD_HOSTNAME=freefire-server
 export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
+export SKIP_ABI_CHECKS=true
 export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=false
+export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
 ccache -M 8G -F 0
 $lunch
-$make bacon -j16 &
+$make -j8 &
+sleep 100m
 kill %1
-ccache -s
+ccache -s 
